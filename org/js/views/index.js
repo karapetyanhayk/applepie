0d26c0fb425fb17a6b32e5fd2604fe8ab7c3456b
Hayk Karapetyan <hayko.karapetov@gmail.com>
2014-02-15 17:36:43 +0400

    js/view/indexJS changes to show firstname lastname

--- org/js/views/index.js
+++ org/js/views/index.js
@@ -1,29 +1,45 @@
 (function() {
-    define(['require', 'jquery', 'underscore', 'bb', 'i/item/c','text!/html/index.html'], function(require, $, _, Backbone, Items) {
+    define(['require', 'jquery', 'underscore', 'bb', 'i/item/c', 'text!/html/index.html'], function(require, $, _, Backbone, Items) {
         return Backbone.View.extend({
             id: 'index',
             initialize: function(options) {
                 var that = this;
                 this.___ = options.___;
-                this.items = new Items(null,{ s: this.___.so});
+                this.items = new Items(null, {
+                    s: this.___.so
+                });
                 var Home = require('text!/html/index.html');
                 this.home = _.template(Home);
                 that.render();
             },
             events: {
+                "click .create": "createItem"
             },
-            render:function(){
-                var that       = this;
+            render: function() {
+                var that = this;
                 that.$el.html(this.home({}))
-               /* that.items.fetch({
+                /* that.items.fetch({
                     success:function(){
                     },data:{}
                 })
              */
-            
+            },
+            createItem: function() {
+                var that = this;
+                this.items.create({
+                    "path": "newobj",
+                    "title": "I'm the Sword Master",
+                    "group": "Dwarfs",
+                    "body": {
+                        "firstname": "Jimmy",
+                        "lastname": "Cimmel"
+                    }
+                }, {
+                    callback: function(json, m) {
+                        that.$(".message").html("created " + m.get("body.firstname") + " " + m.get("body.lastname"));
+                    }
+                })
             }
+        });
     });
-});
-
 }).call(this);
-
